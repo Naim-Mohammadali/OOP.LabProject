e939e8fdbc700ea9bed495d48d39fae2755bbb50
Naim-Mohammadali <mohammadali.naim.official@gmail.com>
2025-02-06 06:30:11 +0330

    Last Last Commit: Packaging the code structure was reshaped to make it a full package with necessary comments

--- src/LabProject/Lab.java
+++ src/LabProject/Lab.java
@@ -1,5 +1,38 @@
 package LabProject;
+// This Package tends to systemize the data in a Classroom- e.g. Advanced Program Lab
+// The package has two main classes: Student & Lab
+// ****
+//Student Class
+class Student {
+    private final String firstname;
+    private final String lastname;
+    private final String id;
+    private double grade;
+// The Student Class takes the first and last name, and the id of the student
+// to create a file for him/her through the constructor
+    public Student(String firstname, String lastname, String id) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.id = id.substring(0,7);
+        this.grade = 0;
+    }
+// This method prints the info of the student in an elegant way
+    public void printStudentInfo() {
+        System.out.println(firstname + " " + lastname +" - ID: "+ id + " - grade: "+grade);
+    }
+// This method sets a grade for the student
+// If the grade given is more than 20, the modulus of the number w.r.t. 20 is taken
+    public void setGrade(double grade) {
+        this.grade = grade % 20 ;
+    }
+// This method returns the grade of the student
+    public double getGrade() {
+        return grade;
+    }
+}
 
+// ****
+//Lab Class
 class Lab {
     private final Student[] students;
     private final String teacherName;
@@ -7,7 +40,8 @@ class Lab {
     private final int maxSize;
     private int currentSize;
     private double averageGrade;
-
+ // The Lab Class takes the teacherNamem, dayOfWeek, and maxSize
+// then initializes a list of students to create a new Lab
     public Lab(String teacherName, String dayOfWeek, int maxSize) {
         this.teacherName = teacherName;
         this.dayOfWeek = dayOfWeek;
@@ -15,9 +49,8 @@ class Lab {
         students = new Student[maxSize];
         currentSize = 0;
     }
-    public void setAverageGrade(double averageGrade) {
-        this.averageGrade = averageGrade;
-    }
+
+// This method calculates the average of Lab-grades
     public void calAvg()
     {
         averageGrade = 0;
@@ -25,8 +58,9 @@ class Lab {
             averageGrade += students[i].getGrade();
         }
         averageGrade /= currentSize;
-        setAverageGrade(averageGrade);
     }
+ // This method enrolls a new student in the Lab if it is not full yet
+ // It checks the avGrade and currentSize are up-to-date
     public void enrollStudent (Student student)
     {
         if (currentSize <= maxSize)
@@ -37,6 +71,7 @@ class Lab {
         }
 
     }
+// This method prints the info of the Lab in an elegant way
     public void printLabInfo()
     {
         System.out.println(
